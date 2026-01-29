1946bfa504c139a81cd2c94b1ba12e686e733eaf
Aaron Dill <aaronsacks2006@gmail.com>
2026-01-29 16:21:29 -0600

    feat: stop.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 shopt -s nullglob
+cmd=(up -d)
+if [[ "$0" == *"stop.sh" ]]; then
+  cmd=(down)
+fi
+
 files=(./*/compose.yaml)
 if [[ "${#files[@]}" -eq 0 ]]; then
   echo "No compose files found" >&2
@@ -9,7 +14,7 @@ fi
 
 for file in "${files[@]}"; do
   dir="$(dirname -- "$file")"
-  pushd "$dir" || continue
-  docker compose up -d
-  popd
+  pushd "$dir" >/dev/null || continue
+  docker compose "${cmd[@]}"
+  popd >/dev/null
 done
