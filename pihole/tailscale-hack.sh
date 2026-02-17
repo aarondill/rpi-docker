e1d703d0ef501813f82eb46fbf21d24fd15bf5de
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-17 01:12:00 -0600

    fix: I give up. It works now.
    Stop being nice and proper. Just set the damn netmask.

---
+++ pihole/tailscale-hack.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while true; do
+  if ! ip monitor dev tailscale0 | head -n1 2>/dev/null; then
+    sleep 5 # the interface doesn't exist yet, wait a bit and try again
+  fi
+  ifconfig tailscale0 netmask 255.192.0.0
+done
