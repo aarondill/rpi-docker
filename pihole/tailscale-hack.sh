b3b37aade0e5ab861f9d3f8e2799b6752bdcd757
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-17 01:23:56 -0600

    fix: use tailscale image since alpine doesn't have needed ip

--- pihole/tailscale-hack.sh
+++ pihole/tailscale-hack.sh
@@ -3,5 +3,6 @@ while true; do
   if ! ip monitor dev tailscale0 | head -n1 2>/dev/null; then
     sleep 5 # the interface doesn't exist yet, wait a bit and try again
   fi
+  echo "Setting tailscale0 netmask"
   ifconfig tailscale0 netmask 255.192.0.0
 done
